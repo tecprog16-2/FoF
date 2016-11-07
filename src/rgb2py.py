f77b68ae3405380e59b0fd38dbd45f15fe703c5f
Lucas Souza <lucas.soaresouza@gmail.com>
2016-11-06 23:52:42 -0200

    Applied 'prioritize clarity over brevity' in the rgb2py.py file

--- src/rgb2py.py
+++ src/rgb2py.py
@@ -11,3 +11,21 @@ for l in f.readlines():
   for n in names:
     print '  %-24s: (%.2f, %.2f, %.2f),' % ('"%s"' % n.lower(), rgb[0], rgb[1], rgb[2])
 print "}"
+
+
+rgbFIle= open("/etc/X11/rgb.txt")
+
+print "colors = {"
+for line in rgbFIle.readlines():
+
+  if line.startswith("!"):
+    continue
+  else
+    #Do nothing
+
+  tax_rgb = re.split("[ \t]+", line.strip())
+  rgbCollor, namesList = map(int, tax_rgb[:3]), tax_rgb[3:]
+  rgbCollor = [float(tax_rgb) / 255.0 for tax_rgb in rgbCollor]
+  for name in namesList:
+    print '  %-24s: (%.2f, %.2f, %.2f),' % ('"%s"' % name.lower(), rgbCollor[0], rgbCollor[1], rgbCollor[2])
+print "}"
